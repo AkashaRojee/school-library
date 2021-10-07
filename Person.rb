dd4f0fd7a83c9a2da983d85131cec0e18060727c
Akasha <Akasha.Rojee@gmail.com>
2021-10-07 18:42:19 +0400

    Create private method to check age

--- Person.rb
+++ Person.rb
@@ -1,10 +1,15 @@
 class Person
   attr_accessor :name, :age
   attr_reader :id
-  
+
   def initialize(name = "Unknown", age, parent_permission = true)
-    @id = Random.rand(0..5000)
+    @id = Random.rand(0..10000)
     @name = name
     @age = age
+  end
+
+  private
+  def is_of_age?
+    age >= 18
   end
 end
