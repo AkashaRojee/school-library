54f011c2788960791753385393e530b684add590
Akasha <Akasha.Rojee@gmail.com>
2021-10-07 19:14:09 +0400

    Create corrector instance in Person initialisation

--- person.rb
+++ person.rb
@@ -1,3 +1,5 @@
+require './corrector'
+
 class Person
   attr_accessor :name, :age
   attr_reader :id
@@ -7,6 +9,7 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
   end
 
   private
