44f3aaa20804d08881834f3c94eb6da327fa5320
Akasha <Akasha.Rojee@gmail.com>
2021-10-07 19:20:40 +0400

    Add parent permission to Teacher constructor

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require './person'
 
 class Teacher < Person
-  def initialize(age, specialization, name = 'Unknown')
-    super(age, name)
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
