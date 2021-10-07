43992deac3d0355a3de7067d933cdffb1c987bc5
Akasha <Akasha.Rojee@gmail.com>
2021-10-07 20:05:20 +0400

    Set classroom of student

--- student.rb
+++ student.rb
@@ -6,6 +6,11 @@ class Student < Person
     @classroom = classroom
   end
 
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self)
+  end
+
   def play_hooky
     "¯\(ツ)/¯"
   end
