72ae94eb5ef48be67f8ace621255c35ec6e0b95b
Akasha <Akasha.Rojee@gmail.com>
2021-10-08 13:34:06 +0400

    Add missing getters and setters

--- person.rb
+++ person.rb
@@ -1,8 +1,8 @@
-require './corrector'
+require_relative './corrector'
 
 class Person
-  attr_accessor :name, :age
-  attr_reader :id, :rentals
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(0..10_000)
