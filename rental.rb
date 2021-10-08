7d96e5e1c9e54f162c1e1b7746090181d28ec971
Akasha <Akasha.Rojee@gmail.com>
2021-10-08 14:21:22 +0400

    Add rental to book and person

--- rental.rb
+++ rental.rb
@@ -3,16 +3,10 @@ class Rental
 
   def initialize(date, book, person)
     @date = date
-    @book = book
-    @person = person
-  end
 
-  def add_book(book)
     @book = book
     book.rentals.push(self)
-  end
 
-  def add_person(person)
     @person = person
     person.rentals.push(self)
   end
