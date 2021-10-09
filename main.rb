ee5c13dd888039de9362f3c88818338e90c3749c
Akasha <Akasha.Rojee@gmail.com>
2021-10-09 15:23:19 +0400

    List all people

--- main.rb
+++ main.rb
@@ -1,9 +1,11 @@
 require_relative './book'
 
 LIST_ALL_BOOKS_CODE = '1'.freeze
+LIST_ALL_PEOPLE_CODE = '2'.freeze
 
 def print_options
   puts "#{LIST_ALL_BOOKS_CODE}) List all books"
+  puts "#{LIST_ALL_PEOPLE_CODE}) List all people"
 end
 
 def main
@@ -13,6 +15,8 @@ def main
     case gets.chomp
     when LIST_ALL_BOOKS_CODE
       books.each { |book| puts book.details }
+    when LIST_ALL_PEOPLE_CODE
+      people.each { |pep| puts pep.details }
     end
   end
 end
